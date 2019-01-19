858c1bcc41c23e1916f9e0daac007c3f34b42954
Joan Williams <joanlinw@gmail.com>
2019-01-18 21:27:38 -0800

    content polishing

---
+++ js/js.js
@@ -0,0 +1,14 @@
+// FOOTER::SOCIAL MEDIA IMAGE ICON CHANGER
+
+$("#instagram-icon-footer") .mouseover(function () {
+    this.src= "imgs/instagram_hover.svg"
+ }).mouseout(function () {
+     this.src= "imgs/instagram_fresh.svg"
+ });
+
+ $("#facebook-icon-footer") .mouseover(function () {
+    this.src= "imgs/facebook_hover.svg"
+ }).mouseout(function () {
+     this.src= "imgs/facebook_fresh.svg"
+ });
+
